bcd0a3902de0a763087c0b1131fcf27313344870
stash4uk <stash4uk@gmail.com>
2023-02-25 02:57:16 -0500

    Added navigation, footer, metatags and og protocol attributes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,32 @@
 function Footer() {
-  return;
+  return (
+    <>
+      <img
+        src={require("../icons_assets/restauranfood.jpg").default}
+        alt="Something visual"
+      />
+      <h3>Doormat Navigation</h3>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Menu</li>
+        <li>Reservations</li>
+        <li>Order Online</li>
+        <li>Login</li>
+      </ul>
+      <h3>Contact</h3>
+      <ul>
+        <li>Address</li>
+        <li>Phone Number</li>
+        <li>Email</li>
+      </ul>
+      <h3>Social Media Links</h3>
+      <ul>
+        <li>Facebook</li>
+        <li>Instagram</li>
+        <li>Twitter</li>
+      </ul>
+    </>
+  );
 }
 export default Footer;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,13 @@
 function Nav() {
-  return;
+  return (
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Menu</li>
+      <li>Reservations</li>
+      <li>Order Online</li>
+      <li>Login</li>
+    </ul>
+  );
 }
 export default Nav;
